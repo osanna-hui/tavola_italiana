57b33a684e94d439371360a48f7076c22b3094fd
Anth0z <anthornato@hotmail.com>
2016-02-22 01:04:29 -0800

    added shopping cart, still php problems duh but that's life forever

--- mod/Parameters.php
+++ mod/Parameters.php
@@ -9,12 +9,24 @@ class Parameters {
     public function __construct($method = "GET") {
 
         if($method === "GET") {
+            
+            // error checking:
+            if(!isset($_GET)) {
+              $values['error'] = 'No $_GET found';
+              return $values;
+            }
 
             foreach ($_GET as $key => $value){
                 $this->values[$key] = $value;
             }
 
         } else if ($method === "POST") {
+            
+            // error checking:
+            if(!isset($_POST)) {
+              $values['error'] = 'No $_POST found';
+              return $values;
+            }
 
             foreach ($_POST as $key => $value){
                 $this->values[$key] = $value;
